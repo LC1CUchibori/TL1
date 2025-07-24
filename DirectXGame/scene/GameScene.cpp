4c3c780fe84b1ea801dedc20f4fce13a28206340
LC1CUchibori <146916712+LC1CUchibori@users.noreply.github.com>
2025-07-24 15:29:16 +0900

    ok

--- DirectXGame/scene/GameScene.cpp
+++ DirectXGame/scene/GameScene.cpp
@@ -1,6 +1,11 @@
 #include "GameScene.h"
 #include "TextureManager.h"
+
+#include <string>
+#include <json.hpp>
+#include <fstream>
 #include <cassert>
+#include <vector>
 
 GameScene::GameScene() {}
 
@@ -11,6 +16,117 @@ void GameScene::Initialize() {
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+
+	// ------------------------------------------------------------
+	// レベルデータを格納するための構造体
+	// ------------------------------------------------------------
+
+	struct ObjectData
+	{
+		std::string type;
+		std::string name;
+
+		struct Transform
+		{
+			Vector3 translation;
+			Vector3 rotation;
+			Vector3 scaling;
+		};
+
+		Transform transform;
+
+		std::string file_name;
+	};
+
+	struct LevelData
+	{
+		std::string name;
+		std::vector<ObjectData>objects;
+	};
+
+	// ------------------------------------------------------------
+	// jsonファイルのデシリアライズ化
+	// ------------------------------------------------------------
+
+	// jsonファイルのパス名
+	const std::string fullpath = std::string("Resource/levels/") + "scene.json";
+
+	// ファイルストリーム
+	std::ifstream file;
+
+	// ファイルを開く
+	file.open(fullpath);
+	// ファイルオープン失敗をチェック
+	if (file.fail()) {
+		assert(0);
+	}
+
+	nlohmann::json deserialized;
+
+	// ファイルから読み込み、メモリへ格納
+	file >> deserialized;
+
+	// 正しいレベルデータファイルかチェック
+	assert(deserialized.is_object());
+
+	assert(deserialized.contains("name"));
+	assert(deserialized["name"].is_string());
+
+	// ------------------------------------------------------------
+	// レベルデータを構造体に格納していく
+	// ------------------------------------------------------------
+
+	LevelData* levelData = new LevelData();
+
+	// "name" を文字列として取得
+	levelData->name = deserialized["name"].get<std::string>();
+	assert(levelData->name == "scene");    // それは "scene" か？
+
+	// "objects" の全オブジェクトを走査
+	for (const nlohmann::json& object : deserialized["objects"]) {
+
+		// オブジェクト1つ分の妥当性のチェック
+		assert(object.contains("type"));    // "type" が含まれているか
+
+		if (object["type"].get<std::string>() == "MESH") {
+
+			// 1個分の要素の準備
+			levelData->objects.emplace_back(ObjectData{});
+			ObjectData& objectData = levelData->objects.back();  // 追加要素の参照を用意して可読性もよくなる
+
+			objectData.type = object["type"].get<std::string>();   // "type"
+			objectData.name = object["name"].get<std::string>();   // "name"
+
+			// トランスフォームのパラメータ読み込み
+			nlohmann::json transform = object["transform"];
+			// 平行移動 "translation"
+			objectData.transform.translation.x = (float)transform["translation"][0];
+			objectData.transform.translation.y = (float)transform["translation"][2];
+			objectData.transform.translation.z = (float)transform["translation"][1];
+			// 回転 "rotation"
+			objectData.transform.rotation.x = -(float)transform["rotation"][0];
+			objectData.transform.rotation.y = -(float)transform["rotation"][2];
+			objectData.transform.rotation.z = -(float)transform["rotation"][1];
+			// 拡縮 "scaling"
+			objectData.transform.scaling.x = (float)transform["scaling"][0];
+			objectData.transform.scaling.y = (float)transform["scaling"][2];
+			objectData.transform.scaling.z = (float)transform["scaling"][1];
+
+			// "file_name"
+			if (object.contains("file_name")) {
+				objectData.file_name = object["file_name"].get<std::string>();
+			}
+		}
+	}
+
+	for (auto& ObjectData : levelData->objects) {
+		Model* model = nullptr;
+
+
+	
+
+
+	}
 }
 
 void GameScene::Update() {}
